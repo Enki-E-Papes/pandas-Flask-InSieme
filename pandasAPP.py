38f7cb72bf3de4a63095373f36b7374ff044321e
Lich-senpai13 <papesenki.enrico@iisgalvanimi.edu.it>
2023-04-12 12:55:07 +0000

    cioa

--- pandasAPP.py
+++ pandasAPP.py
@@ -6,10 +6,8 @@ app = Flask(__name__)
 def a():
     import pandas as pd
     film = pd.read_csv('https://raw.githubusercontent.com/wtitze/3E/main/2010.csv',sep=";")
-    film
-    return render_template('index.html', film = film)
-
-
+    film2 = film[film["Language"].str.contains("English")]
+    return render_template('index.html', film2 = film2.to_html())
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
